cc8257226a2ad65e6ab713ca35682301171a4046
Binbasri-in <binbasri.india@gmail.com>
2022-11-10 23:49:35 +0530

    Add try 1 file

--- task2.2/try1.py
+++ task2.2/try1.py
@@ -20,12 +20,12 @@
 # Team ID:			[ Team-ID ]
 # Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
 # Filename:			percepStack.py
-# Functions:		
+# Functions:
 # 					[ Comma separated list of functions in this file ]
 
 
 ####################### IMPORT MODULES #######################
-import cv2 
+import cv2
 import rospy
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
@@ -36,6 +36,9 @@ import numpy as np
 
 
 # Initialize Global variables
+pub_rgb = rospy.Publisher('/center_rgb', String, queue_size=1)
+pub_depth = rospy.Publisher('/center_depth', String, queue_size=1)
+pose_g = list()
 
 
 ################# ADD UTILITY FUNCTIONS HERE #################
@@ -57,34 +60,45 @@ def img_clbck(img_msg):
     -----
     img_msg: Callback message.
     '''
-    global pub_rgb #, add global variable if any
+    global pub_rgb  # , add global variable if any
 
     ############################### Add your code here #######################################
+    # Convert the image to cv2 format
+    bridge = CvBridge()
+    image = bridge.imgmsg_to_cv2(img_msg, "bgr8")
 
+    # show the image
+    cv2.imshow("Image 2window", image)
+    cv2.waitKey(0)
     ##########################################################################################
     pose = image_processing(image)
+    global pose_g
+    pose_g = pose
     pub_rgb.publish(str(pose))
+
 
 def depth_clbck(depth_msg):
     '''
     Callback Function for Depth image topic
 
     Purpose:
-	--- 
+        --- 
     1. Find the depth value of the centroid pixel returned by the
     image_processing() function.
     2. Publish the depth value to the topic '/center_depth'
 
 
     NOTE: the shape of depth and rgb image is different. 
-    
+
     Input Args:
     -----
     depth_msg: Callback message.
     '''
     depth_val = []
     ############################### Add your code here #######################################
-
+    # print the message to check the format of the message
+    global pose_g
+    rospy.loginfo(f"Depth: {pose_g}")
     ##########################################################################################
     pub_depth.publish(str(depth_val))
 
@@ -95,7 +109,7 @@ def image_processing(image):
           Only do the changes in the specified portion for this
           function.
           Use cv2.imshow() for debugging but make sure to REMOVE it before submitting.
-    
+
     1. Find the centroid of the bell pepper(s).
     2. Add the x and y values of the centroid to a list.  
     3. Then append this list to the pose variable.
@@ -111,10 +125,27 @@ def image_processing(image):
     '''
     pose = []
     ############### Write Your code to find centroid of the bell peppers #####################
-
+    # Convert the image to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # Convert the image to HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    # Define the lower and upper range of the color
+    lower = np.array([0, 100, 100])
+    upper = np.array([10, 255, 255])
+    # Create a mask
+    mask = cv2.inRange(hsv, lower, upper)
+    # Find the contours
+    contours, hierarchy = cv2.findContours(
+        mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Find the centroid of the contours
+    for cnt in contours:
+        M = cv2.moments(cnt)
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+            pose.append([cx, cy])
     ##########################################################################################
     return pose
-
 
 
 def main():
@@ -132,24 +163,29 @@ def main():
     '''
 
     #### EDIT YOUR CODE HERE FOR SUBSCRIBING TO OTHER TOPICS AND TO APPLY YOUR ALGORITHM TO PUBLISH #####
-
+    cv2.destroyAllWindows()
     rospy.init_node("percepStack", anonymous=True)
-    sub_image_color_1 = rospy.Subscriber("/device_0/sensor_1/Color_0/image/data_1", Image, img_clbck)
-    sub_image_depth_1 = rospy.Subscriber("/device_0/sensor_0/Depth_0/image/data_1", Image, depth_clbck)
+    # subscribe to image topic 1
+    sub_image_color_1 = rospy.Subscriber(
+        "/device_0/sensor_1/Color_0/image/data_1", Image, img_clbck)
+    sub_image_depth_1 = rospy.Subscriber(
+        "/device_0/sensor_0/Depth_0/image/data_1", Image, depth_clbck)
 
-    sub_image_color_2 = rospy.Subscriber("/device_0/sensor_1/Color_0/image/data_2", Image, img_clbck)
-    sub_image_depth_2 = rospy.Subscriber("/device_0/sensor_0/Depth_0/image/data_2", Image, depth_clbck)
+    # subscribe to image topic 2
+    sub_image_color_2 = rospy.Subscriber(
+        "/device_0/sensor_1/Color_0/image/data_2", Image, img_clbck)
+    sub_image_depth_2 = rospy.Subscriber(
+        "/device_0/sensor_0/Depth_0/image/data_2", Image, depth_clbck)
 
-    sub_image_color_3 = rospy.Subscriber("/device_0/sensor_1/Color_0/image/data_3", Image, img_clbck)
-    sub_image_depth_3 = rospy.Subscriber("/device_0/sensor_0/Depth_0/image/data_3", Image, depth_clbck)
-
-
-
-    pub_rgb = rospy.Publisher('/center_rgb', String, queue_size = 1)
-    pub_depth = rospy.Publisher('/center_depth', String, queue_size = 1)
+    # subscribe to image topic 2
+    sub_image_color_3 = rospy.Subscriber(
+        "/device_0/sensor_1/Color_0/image/data_3", Image, img_clbck)
+    sub_image_depth_3 = rospy.Subscriber(
+        "/device_0/sensor_0/Depth_0/image/data_3", Image, depth_clbck)
 
     ####################################################################################################
     rospy.spin()
+
 
 if __name__ == '__main__':
     try:
