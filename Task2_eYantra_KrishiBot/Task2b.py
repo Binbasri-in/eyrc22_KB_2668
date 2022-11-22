a6c48ec6ae20df4fad1f0deeb6fec0215d07c96c
Binbasri-in <binbasri.india@gmail.com>
2022-11-22 20:46:45 +0530

    Delete the trial files and keep the original

--- Task2_eYantra_KrishiBot/Task2b.py
+++ Task2_eYantra_KrishiBot/Task2b.py
@@ -41,7 +41,46 @@ pub_depth = rospy.Publisher('/center_depth', String, queue_size=1)
 pose_g = []
 
 ################# ADD UTILITY FUNCTIONS HERE #################
+# a function to reduce the noise in the image
 
+
+def reduce_noise(img):
+    # clean up
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+    mask_red_closed = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    mask_red_closed_then_opened = cv2.morphologyEx(
+        mask_red_closed, cv2.MORPH_OPEN, kernel)
+    return mask_red_closed_then_opened
+
+# function to get the centroid of the image
+
+
+def get_centroid(img):
+    M = cv2.moments(img)
+    if M['m00'] != 0:
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        return cx, cy
+    else:
+        return 0, 0
+
+# a function to get the biggest contour and return a mask of that contour
+
+
+def get_biggest_contour(img):
+    # get the contours
+    contours, hierarchy = cv2.findContours(
+        img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # check if there is any contour
+    if len(contours) != 0:
+        # get the biggest contour
+        biggest_contour = max(contours, key=cv2.contourArea)
+        # create a mask of the biggest contour
+        mask = np.zeros(img.shape, np.uint8)
+        cv2.drawContours(mask, [biggest_contour], 0, 255, -1)
+    else:
+        mask = img
+    return mask
 ##############################################################
 
 
@@ -60,11 +99,12 @@ def img_clbck(img_msg):
     img_msg: Callback message.
     '''
     global pub_rgb  # , add global variable if any
+
     ############################### Add your code here #######################################
     # Convert the image to cv2 format
     bridge = CvBridge()
     try:
-        image = bridge.imgmsg_to_cv2(img_msg, "bgr8")
+        image = bridge.imgmsg_to_cv2(img_msg, "passthrough")
     except CvBridgeError as e:
         rospy.loginfo(f"Error: {e}")
 
@@ -72,6 +112,7 @@ def img_clbck(img_msg):
     if image is None:
         return
     #rospy.loginfo(f"image shape: {image.shape}")
+
     ##########################################################################################
     pose = image_processing(image)
 
@@ -140,63 +181,40 @@ def image_processing(image):
     '''
     pose = []
     ############### Write Your code to find centroid of the bell peppers #####################
-    # convert the image to RGB
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # blur the image
+    blur = cv2.GaussianBlur(image, (7, 7), 0)
+    # convert to HSV color space
+    hsv = cv2.cvtColor(blur, cv2.COLOR_RGB2HSV)
 
-    # Remove noise from the image
-    # define the kernel size
-    kernel_size = 5
-    image = cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
+    # start with red color mask
+    min_red = np.array([170, 100, 80])
+    max_red = np.array([180, 256, 256])
+    # Threshold the HSV image to get only yellow colors
+    mask_red = cv2.inRange(hsv, min_red, max_red)
+    # reduce noise
+    mask_clean = reduce_noise(mask_red)
+    # get the mask of the biggest contour
+    mask_biggest_contour = get_biggest_contour(mask_clean)
+    # get the centroid of the biggest contour
+    cx, cy = get_centroid(mask_biggest_contour)
+    # if the centroid is not (0,0) then append it to the center list
+    if cx != 0 and cy != 0:
+        pose.append([cx, cy])
 
-    # convert the image to HSV
-    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-
-    # Filter the image using blue color
-    # define two blue boundaries, one for red and one for golden yellow
-    # for red
-    blueLower1 = np.array([166, 100, 100])
-    blueUpper1 = np.array([186, 255, 255])
-    # for golden yellow
-    blueLower2 = np.array([5, 100, 100])
-    blueUpper2 = np.array([25, 255, 255])
-    # Threshold the HSV image to get only blue colors
-    mask1 = cv2.inRange(hsv, blueLower1, blueUpper1)
-    mask2 = cv2.inRange(hsv, blueLower2, blueUpper2)
-    # combine the two masks
-    mask = mask1 | mask2
-
-    # save the dimentions of the mask in variable
-    height, width = mask.shape
-    # create an image with the same dimentions as the mask
-    mask_image = np.zeros((height, width, 3), np.uint8)
-    # fill the image with the color white
-    mask_image[:] = (255, 255, 255)
-    # Bitwise-AND mask and masked image
-    res = cv2.bitwise_and(mask_image, mask_image, mask=mask)
-
-    # use Morphological Transformations to remove noise
-    # define the kernel size
-    kernel = np.ones((5, 5), np.uint8)
-    # apply the erosion
-    erosion = cv2.erode(res, kernel, iterations=12)
-    # apply dilation
-    dilation = cv2.dilate(erosion, kernel, iterations=12)
-
-    # create a mask from the image
-    mask = cv2.inRange(dilation, (254, 254, 254), (255, 255, 255))
-
-    #cv2.imwrite(f"/home/binbasri0/catkin_ws/src/eyrc-2022_krishibot/scripts/original{counter}.png", mask)
-    # find the contours in the mask
-    contours, hierarchy = cv2.findContours(
-        mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # get the center of each contour
-    for c in contours:
-        # calculate moments for each contour
-        M = cv2.moments(c)
-        # calculate x,y coordinate of center
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-        pose.append([cX, cY])
+    # now do the same for yellow color
+    min_yellow = np.array([10, 150, 150])
+    max_yellow = np.array([19, 256, 256])
+    # Threshold the HSV image to get only yellow colors
+    mask_yellow = cv2.inRange(hsv, min_yellow, max_yellow)
+    # reduce noise
+    mask_clean = reduce_noise(mask_yellow)
+    # get the mask of the biggest contour
+    mask_biggest_contour = get_biggest_contour(mask_clean)
+    # get the centroid of the biggest contour
+    cx, cy = get_centroid(mask_biggest_contour)
+    # if the centroid is not (0,0) then append it to the center list
+    if cx != 0 and cy != 0:
+        pose.append([cx, cy])
         #cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
         # write a text for centroid
         #cv2.putText(image, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
@@ -226,6 +244,7 @@ def main():
     rospy.init_node("percepStack", anonymous=True)
     # define a rate
     #rate = rospy.Rate(4)
+
     # start looping
     # while not rospy.is_shutdown():
     # data 1
@@ -248,6 +267,7 @@ def main():
 
     ####################################################################################################
     rospy.spin()
+
 
 if __name__ == '__main__':
     try:
