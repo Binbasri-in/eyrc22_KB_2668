fb10b977dd57b2b6eac2c2d32aa08532828f02ad
Mohammed Ali Alsakkaf (Binbasri) <87440507+Binbasri-in@users.noreply.github.com>
2022-12-05 05:34:33 -0800

    Update percepClass.py

--- task3/percepClass.py
+++ task3/percepClass.py
@@ -30,6 +30,10 @@ class PerceptionNode:
         self.focal_length = [554.3827128226441, 554.3827128226441]
         # center of the image cx, cy
         self.center = [320.5, 240.5]
+        # the value to be published continuously
+        self.two_found = 0
+        self.red_pose = []
+        self.yellow_pose = []
 
     def start_subscribers(self):
         # subscribe to the image topic
@@ -48,7 +52,7 @@ class PerceptionNode:
             return
         
         # call the image processing function
-        self.image_processing(image)
+        self.two_found = self.image_processing(image)
 
     # this function will have the main task where the final values will be calculated and saved in their respected list
     def depth_clbck(self, depth_msg):
@@ -64,8 +68,26 @@ class PerceptionNode:
         if depth_image is None:
             return
 
-        # get the depth and it is x, y pixels
+        # get the latest centroid of the red object
+        if len(self.red_poses) != 0:
+            red_pose = self.red_poses[-1]
+            # get the depth value of the centroid pixel
+            depth_val = depth_image[red_pose[0]*640 // 800, red_pose[1]*480 // 600]
+            # save the depth value
+            self.red_poses[-1].append(depth_val)
 
+        # get the latest centroid of the yellow object
+        if len(self.yellow_poses) != 0:
+            yellow_pose = self.yellow_poses[-1]
+            # get the depth value of the centroid pixel
+            depth_val = depth_image[yellow_pose[0]*640 // 800, yellow_pose[1]*480 // 600]
+            # save the depth value
+            self.yellow_poses[-1].append(depth_val)
+
+        # save the last red and yellow pose
+        if self.two_found == 2:
+            self.red_pose = self.red_poses[-1]
+            self.yellow_pose = self.yellow_poses[-1]
         ##########################################################################################
 
 
@@ -112,6 +134,8 @@ class PerceptionNode:
 
     # image processing function
     def image_processing(self, image):
+        # return value is count
+        count = 0
         blur = cv2.GaussianBlur(image, (7, 7), 0)
         # convert to HSV color space
         hsv = cv2.cvtColor(blur, cv2.COLOR_RGB2HSV)
@@ -130,6 +154,7 @@ class PerceptionNode:
         # if the centroid is not (0,0) then append it to the center list
         if cx != 0 and cy != 0:
             self.red_poses.append([cx, cy])
+            count += 1
 
         # now do the same for yellow color
         min_yellow = np.array([10, 150, 150])
@@ -145,39 +170,42 @@ class PerceptionNode:
         # if the centroid is not (0,0) then append it to the center list
         if cx != 0 and cy != 0:
             self.yellow_poses.append([cx, cy])
+            count += 1
+
+        return count
 
     # broadcast the transform of the camera frame to the world frame (base_link)
     def broadcast_tf(self):
         # get coordinates of red object
-        if len(self.red_poses) != 0:
+        if self.two_found == 2:
             # get the last element in the list
-            red_pose = self.red_poses[-1]
+            red_pose = self.red_pose
             # convert the pixel coordinates to the camera frame
             red_pose = self.pixel2cam(red_pose)
             # broadcast the transform
             self.pub_tf.sendTransform(red_pose, (0, 0, 0, 1), rospy.Time.now(),
-                                      "red_object", "camera_link")
+                                      "fruit_red", "ebot_base")
 
         # get coordinates of yellow object
-        if len(self.yellow_poses) != 0:
+        if self.two_found == 2:
             # get the last element in the list
-            yellow_pose = self.yellow_poses[-1]
+            yellow_pose = self.yellow_pose
             # convert the pixel coordinates to the camera frame
             yellow_pose = self.pixel2cam(yellow_pose)
             # broadcast the transform
             self.pub_tf.sendTransform(yellow_pose, (0, 0, 0, 1), rospy.Time.now(),
-                                      "yellow_object", "camera_link")
+                                      "fruit_yellow", "ebot_base")
 
     # function to convert pixel coordinates to camera frame
     def pixel2cam(self, pixel):
         # get the depth value of the centroid pixel
-        depth_val = self.depth_val
+        depth_val = pixel[2]
         # get the focal length of the camera
         f = self.focal_length
         # get the center of the image
         center = self.center
         # get the pixel coordinates
-        x, y = pixel
+        x, y = pixel[0], pixel[1]
         # calculate the coordinates of the object in the camera frame
         x = (x - center[0]) * depth_val / f
         y = (y - center[1]) * depth_val / f
