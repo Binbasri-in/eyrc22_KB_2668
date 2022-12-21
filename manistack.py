5ce60b77668e8ead98fae0c1696f6449881990cf
Sai Harshit.B <89305971+Rostemelov@users.noreply.github.com>
2022-12-21 23:06:12 +0530

    This code contains the arm manipulation code.
    It contains the functions that moves the arm when pose or predefined pose is passed

---
+++ manistack.py
@@ -0,0 +1,106 @@
+#! /usr/bin/env python3
+import rospy
+import sys
+import copy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import actionlib
+import math
+import tf 
+import numpy as np
+
+class Ur5Moveit:
+
+
+    lx=np.array([[1,0,0],[0 , 0.091866 , 0.99577],[0 , -0.99577 , 0.091866]])
+    ly=np.array([[0.9999997 , 0 , -0.00080285],[0,1,0],[0.00080285 , 0 , 0.9999997]])
+    lz=np.array([[0.9999987 , 0.001588 , 0],[-0.001588 , 0.9999987 , 0],[0,0,1]])
+    left_look=[lx,ly,lz]
+#------------------------------Constructor--------------------------------------------------------
+    def __init__(self):
+
+        rospy.init_node('task3', anonymous=True)
+
+        self._commander = moveit_commander.roscpp_initialize(sys.argv)
+        self._robot = moveit_commander.RobotCommander()
+        self._scene = moveit_commander.PlanningSceneInterface()
+
+        self.arm = moveit_commander.MoveGroupCommander("arm")
+        self.gripper = moveit_commander.MoveGroupCommander("gripper")
+
+        self._display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=5)
+        self._exectute_trajectory_client = actionlib.SimpleActionClient('execute_trajectory', moveit_msgs.msg.ExecuteTrajectoryAction)
+        self._exectute_trajectory_client.wait_for_server()
+
+        self._planning_frame = self.arm.get_planning_frame()
+        self._eef_link = self.arm.get_end_effector_link()
+        self._group_names = self._robot.get_group_names()
+        self._box_name = ''
+
+        # Current State of the Robot is needed to add box to planning scene
+        self._curr_state = self._robot.get_current_state()
+        rospy.loginfo('\033[94m' + " >>> Ur5Moveit init done." + '\033[0m')
+
+#---------------------------------------------------------------------------------------------------------
+#Function to go to pose obtained by camera.
+    def go_to_pose(self, arg_pose):
+
+        pose_values = self.arm.get_current_pose().pose     
+        self.arm.set_pose_target(arg_pose)
+        flag_plan = self.arm.go(wait=True)  # wait=False for Async Move
+        pose_values = self.arm.get_current_pose().pose
+        list_joint_values = self.arm.get_current_joint_values()
+        rospy.loginfo(list_joint_values)
+
+        if (flag_plan == True):
+            rospy.loginfo('\033[94m' + ">>> go_to_pose() Success" + '\033[0m')
+        else:
+            rospy.logerr('\033[94m' + ">>> go_to_pose() Failed. Solution for Pose not Found." + '\033[0m')
+
+        return flag_plan
+#--------------------------------------------------------------------------------------------------
+#Function to go to a predefined pose 
+
+
+    def go_to_predefined_pose(self, group, arg_pose_name):
+
+        rospy.loginfo('\033[94m' + "Going to Pose: {}".format(arg_pose_name) + '\033[0m')
+        group.set_named_target(arg_pose_name)
+        plan = group.plan()
+        goal = moveit_msgs.msg.ExecuteTrajectoryGoal()
+        try:
+            goal.trajectory = plan[1]
+        except:
+           goal.trajectory = plan
+        self._exectute_trajectory_client.send_goal(goal)
+        self._exectute_trajectory_client.wait_for_result()
+        rospy.loginfo('\033[94m' + "Now at Pose: {}".format(arg_pose_name) + '\033[0m')
+    
+#---------------------------------- Destructor-----------------------------------------------------
+    def __del__(self):
+        moveit_commander.roscpp_shutdown()
+        rospy.loginfo(
+            '\033[94m' + "Object of class Ur5Moveit Deleted." + '\033[0m')
+#---------------------------------END OF UR5 CLASS-------------------------------------------------
+
+def main():
+
+    ur5 = Ur5Moveit()
+
+    
+    pose_red = geometry_msgs.msg.Pose()
+    pose_yellow= geometry_msgs.msg.Pose()
+    
+    ur5.go_to_predefined_pose(ur5.arm,"look")
+    lister=tf.TransformListener()
+    flag = 0
+    while not rospy.is_shutdown():
+        (red_trans,red_orient)=lister.lookupTransform('/ebot_base','/fruit_red',rospy.Time(0))
+        (yellow_trans,yellow_orient)=lister.lookupTransform('/ebot_base','/fruit_yellow',rospy.Time(0))
+    del ur5
+
+#----------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    main()
